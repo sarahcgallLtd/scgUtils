c1e2664c2ec1902a772b386794d97f430f246209
Sarah C Gall <sarahcgall@outlook.com>
2023-12-02 12:39:40 +0000

    Fixed issue with grp_freq and grp_mean, added to readme, and amended plots

--- data-raw/colours.R
+++ data-raw/colours.R
@@ -1,9 +1,6 @@
-# LIBRARIES
-library("tidyverse")
-
 # Read csv
-colours <- read.csv("data-raw/colours.csv", encoding = "UTF-8")
-colours <- rename(colours, palette = X.U.FEFF.palette)
+colours <- utils::read.csv("data-raw/colours.csv", encoding = "UTF-8")
+colours <- dplyr::rename(colours, palette = X.U.FEFF.palette)
 
 # save to .rds
 usethis::use_data(colours, internal = TRUE, overwrite = TRUE)
