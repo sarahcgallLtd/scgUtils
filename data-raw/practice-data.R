49b7585db3c207612f6bde0012f419b6b6a6ea0a
sarahcgall <contact@sarahcgall.co.uk>
2024-01-09 15:44:40 +0000

    ignore other

--- data-raw/practice-data.R
+++ data-raw/practice-data.R
@@ -3,7 +3,7 @@
 # https://www.britishelectionstudy.com/data-object/wave-25-of-the-2014-2023-british-election-study-internet-panel/
 
 # read SPSS file and save it in a data frame called "df"
-survey <- haven::read_sav("data-raw/BES2019_W25_v25.0.sav")
+survey <- haven::read_sav("other/BES2019_W25_v25.0.sav")
 
 # Limit size to n=5000 and number of variables
 survey <- survey[1:5000,c(1,4:14,23:35,59:60,73:79,522:525,527,531:553,563,568:568,570)]
